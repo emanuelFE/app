7842bd679f2d185f65af617b0df650092d9d5871
EDILSON JESUS DA SILVA <edilson.jsilva7@senacsp.edu.br>
2022-05-11 16:55:33 -0300

    Segundo projeto em react

---
+++ segundo/src/Main.js
@@ -0,0 +1,24 @@
+function Main() {
+  return (
+    <div className="main">
+      <h4>Bem-Estar</h4>
+      <ul>
+        <li>Reiki-Master</li>
+        <li>Reiki-Nível 1 e 2</li>
+        <li>Reiki-Nível 3</li>
+      </ul>
+
+      <h4>Gestão e Negócios</h4>
+      <ul>
+        <li>Analista Fiscal</li>
+        <li>Assistente Fiscal</li>
+      </ul>
+      <img
+        className="img"
+        alt="imagem reiki"
+        src="https://images.unsplash.com/photo-1564513290352-53b18ee0c797?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+      />
+    </div>
+  );
+}
+export default Main;
