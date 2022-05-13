1a77c136e1509547e16e11fb10d1f47c9d3a9890
EMANOEL FELIPE POLIDORIO DA SILVA <emanoel.fpsilva@senacsp.edu.br>
2022-05-13 17:37:57 -0300

    criançao do quarto

---
+++ quarto/src/components/header/Marca.js
@@ -0,0 +1,11 @@
+import Logo from '../../img/Logo(3).png'
+export default function Marca(){
+
+    return(
+        <div>
+            <img src={Logo} alt="logo marca" />
+        </div>
+    );
+        
+    
+};
---
+++ quarto/src/components/header/Titulo.js
@@ -0,0 +1,9 @@
+export default function Titulo(props) {
+
+    return(
+        <div>
+            <h1>loja tudo aki</h1>
+            <h2>Bem vindos {props.nome}</h2>
+        </div>
+    );
+}
