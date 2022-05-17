a218c355959665e234358671ccbb7fae8f1b94d5
EMANOEL FELIPE POLIDORIO DA SILVA <emanoel.fpsilva@senacsp.edu.br>
2022-05-17 17:20:53 -0300

    criançao do quarto

--- quarto/src/components/main/Container.js
+++ quarto/src/components/main/Container.js
@@ -1,12 +1,37 @@
-
+import {useState,useEffect} from "react";
 import Conteudo from "./Conteudo";
 import Mensagem from "./Mensagem";
 
 export default function Container(){
+
+    // vamos o estado Inicial dos produtos , ou seja
+    // quais dados existem dentro do objeto produtos
+    const [produtos,setProduto] = useState([
+        {
+            id: "",
+            nome: "",
+            descricao: "",
+            categoria: "",
+            preco: "",
+            msg: []
+        }
+    ]);
+
+    useEffect(()=>{
+        
+        fetch("http://10.26.49.52:4000/api/services/produto/listar")
+        .then((response)=> response.json())
+        .then((dados)=>{
+            setProduto(dados.output);
+        })
+        .catch((err)=>console.error(`error ao carregar a api ->${erro}`))
+
+    },[])
+
     return(
         <div className="container">
-            <Mensagem/>
-            <Conteudo/>
+            <Mensagem />
+            <Conteudo dados = {produtos}/>
         </div>
     )
 }
--- quarto/src/components/main/Conteudo.js
+++ quarto/src/components/main/Conteudo.js
@@ -1,8 +1,34 @@
-export default function Conteudo(){
+export default function Conteudo(props){
     return(
         <div className="conteudo">
-            <h2>Conteudo do site</h2>
-            <p>Texto do conteúdo</p>
+            <h2>Produtos</h2>
+
+            <div className="produtos">
+                {
+                    props.dados.map((intens,ix)=>(
+                        <div key={ix}>
+                            <h3>{intens.nome}</h3>
+                            <p>{intens.descricao}</p>
+                            <p>{intens.categoria}</p>
+                            <p className="preco">R$ {intens.preco}</p>
+                            </div>
+                    ))
+                }
+
+                
+                
+                
+                </div>
+
+
+
+
+
+
+
+
+
+            
         </div>
     );
 }
